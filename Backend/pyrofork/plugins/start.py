0f085cf6f6a244d6b37b1de7b75944fc25c354fc
kartal94 <146070323+kartal94@users.noreply.github.com>
2025-11-19 22:34:48 +0300

    Update start.py

--- Backend/pyrofork/plugins/start.py
+++ Backend/pyrofork/plugins/start.py
@@ -11,7 +11,7 @@ async def send_start_message(client: Client, message: Message):
 
         await message.reply_text(
             '<b>Telegram stremio botuna hoş geldin.</b>\n\n'
-            'Stremio eklentisini yüklemek için aşağıdaki URL'yi kopyalayın ve Stremio eklentilerine ekleyin:\n\n'
+            'Stremio eklentisini yüklemek için aşağıdaki URLyi kopyalayın ve Stremio eklentilerine ekleyin:\n\n'
             f'<b>Eklenti adresin:</b>\n<code>{addon_url}</code>',
             quote=True,
             parse_mode=enums.ParseMode.HTML
