424b583690b1b46b7704ea1fd5663a72cda8699d
kartal788 <146070323+kartal788@users.noreply.github.com>
2025-12-10 17:03:20 +0300

    Update and rename vtindir.py to vindir.py

--- Backend/pyrofork/plugins/vindir.py
+++ Backend/pyrofork/plugins/vindir.py
@@ -3,6 +3,6 @@ from pyrogram.types import Message
 from Backend.helper.custom_filter import CustomFilters  # Owner filtresi burada
 
 # ---------------- /vtindir Komutu ----------------
-@Client.on_message(filters.command("vtindir") & filters.private & CustomFilters.owner)
+@Client.on_message(filters.command("vindir") & filters.private & CustomFilters.owner)
 async def vtindir_handler(client: Client, message: Message):
     await message.reply_text("Merhaba")
